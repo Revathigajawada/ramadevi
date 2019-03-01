7d31c49a1a76b9a853a781d71ef459cd2f85cae7
QAP19 <QAP19@QAP19-PC>
2019-03-01 12:25:24 +0530

    First commit

---
+++ sai suppu/src/sp/spr.java
@@ -0,0 +1,19 @@
+package sp;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class spr {
+	public class sai {
+		@Test
+		public void deleteEmp() {
+			Assert.assertEquals(11, 11);
+		}
+		@Test
+		public void addLocation() {
+			Assert.assertEquals(11, 11);
+		}
+	
+	}
+}
+	
